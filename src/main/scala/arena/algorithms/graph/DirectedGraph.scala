8fcbd88b7511d2a1c470098e2224e5cf966d1bce
Bhupendra Bhudia <bhupendra.bhudia@gmail.com>
2015-11-26 16:07:37 +0000

    Minor updates to DirectedGraph

--- src/main/scala/arena/algorithms/graph/DirectedGraph.scala
+++ src/main/scala/arena/algorithms/graph/DirectedGraph.scala
@@ -24,6 +24,13 @@ class DirectedGraph[N, V] extends Iterable[N] {
     }
   }
 
+  /**
+   * Return the weight of the edge between a start node and a destination.
+   * If the edge does not exist, it returns an None.
+   *
+   * @param start The start node.
+   * @param dest  The destination node.
+   */
   def weightOf(start: N, dest: N): Option[V] = {
     if (graphMap.contains(start) && graphMap.contains(dest))
       Option[V](graphMap(start)(dest))
@@ -31,53 +38,45 @@ class DirectedGraph[N, V] extends Iterable[N] {
       None
   }
 
+  /**
+   * Returns an immutable set of the nodes in the graph.
+   *
+   * @return Returns an immutable set of the nodes in the graph.
+   */
   def nodes: Set[N] = graphMap.keys.toSet
 
   /**
-   * Adds a new node to the graph.  If the node already exists, this
-   * function is a no-op.
+   * Adds a new node to the graph. If the node already exists, this function is a no-op.
    *
    * @param node The node to add.
    * @return Whether or not the node was added.
    */
-  def addNode(node: N): Boolean = {
-    if (graphMap.contains(node))
-      false
-    else {
+  def addNode(node: N) = {
+    if (!graphMap.contains(node))
       graphMap(node) = mutable.HashMap()
-      true
-    }
   }
 
   /**
    * Given a start node, and a destination, adds an arc from the start node
-   * to the destination.  If an arc already exists, this operation is a
-   * no-op.  If either endpoint does not exist in the graph, throws a
-   * NoSuchElementException.
+   * to the destination. If an edge already exists, this operation is an update.
    *
    * @param start The start node.
    * @param dest  The destination node.
    * @param weight The weight or distance between start and destination node.
-   * @throws java.util.NoSuchElementException If either the start or destination nodes do not exist.
    */
   def addEdge(start: N, dest: N, weight: V) = {
-    if (!graphMap.contains(start))
-      graphMap(start) = mutable.HashMap()
-
-    if (!graphMap.contains(dest))
-      graphMap(dest) = mutable.HashMap()
+    addNode(start)
+    addNode(dest)
 
     graphMap(start) += dest -> weight
   }
 
   /**
-   * Removes the edge from start to dest from the graph.  If the edge does
-   * not exist, this operation is a no-op.  If either endpoint does not
-   * exist, this throws a NoSuchElementException.
+   * Removes the edge from start to dest from the graph.  If the edge does not exist, this operation is a no-op.
+   * If either endpoint does not exist, this is a no-op.
    *
    * @param start The start node.
    * @param dest  The destination node.
-   * @throws java.util.NoSuchElementException If either node is not in the graph.
    */
   def removeEdge(start: N, dest: N) = {
     if (graphMap.contains(start) && graphMap.contains(dest))
@@ -87,12 +86,11 @@ class DirectedGraph[N, V] extends Iterable[N] {
   /**
    * Given two nodes in the graph, returns whether there is an edge from the
    * first node to the second node.  If either node does not exist in the
-   * graph, throws a NoSuchElementException.
+   * graph, this returns false.
    *
    * @param start The start node.
    * @param end   The destination node.
    * @return Whether there is an edge from start to end.
-   * @throws java.util.NoSuchElementException If either endpoint does not exist.
    */
   def edgeExists(start: N, end: N): Boolean = {
     if (!graphMap.contains(start) || !graphMap.contains(end))
@@ -107,7 +105,6 @@ class DirectedGraph[N, V] extends Iterable[N] {
    *
    * @param node The node whose edges should be queried.
    * @return An immutable view of the edges leaving that node.
-   * @throws java.util.NoSuchElementException If the node does not exist.
    */
   def edgesFrom(node: N): Set[N] = graphMap(node).keys.toSet
 
